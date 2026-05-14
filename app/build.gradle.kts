a19f990f89fd66618a909516874411b3124d3344
Rohan Singh <itzrohanrajput@gmail.com>
2026-05-14 13:47:08 +0530

    Fix AAR metadata issue: set compileSdk to 36 to satisfy dependency requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "com.designthinkingpractical.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.designthinkingpractical.app"
